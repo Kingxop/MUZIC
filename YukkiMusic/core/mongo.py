e4b2fb7a0782baa10ee685ceac569739866f6fbd
Kingxop <95347735+Kingxop@users.noreply.github.com>
2022-11-22 16:35:11 +0530

    Update mongo.py

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,7 +15,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://techzbots:music@cluster0.nsihe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://OMEGAxKING:OMEGAxKING@cluster0.wqfjzmq.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
